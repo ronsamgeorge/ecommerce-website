a2b32ae0d5490a530b09a48731bcd37df506eef7
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-17 13:42:38 +0530

    Add Ui Navbar and Announcement Components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Annoucement from "../components/Annoucement";
+import Navbar from "../components/Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Annoucement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
