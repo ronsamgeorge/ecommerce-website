4225269c652405bb7e4550da3a7513b5aec31194
Ronald George <ronaldsamgeorge@gmail.com>
2023-01-22 00:25:01 +0530

    Add media responsiveness for page : Cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -4,11 +4,13 @@ import Navbar from "../components/Navbar";
 import Annoucement from "../components/Annoucement";
 import Footer from "../components/Footer";
 import { Add, Remove } from "@material-ui/icons";
+import { mobile } from "../responsive";
 
 const Container = styled.div``;
 
 const Wrapper = styled.div`
   padding: 20px;
+  ${mobile({ padding: "10px" })};
 `;
 
 const Title = styled.div`
@@ -33,7 +35,9 @@ const TopButton = styled.button`
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
-const TopTexts = styled.div``;
+const TopTexts = styled.div`
+  ${mobile({ display: "none" })};
+`;
 
 const TopText = styled.span`
   text-decoration: underline;
@@ -44,6 +48,8 @@ const TopText = styled.span`
 const Bottom = styled.div`
   display: flex;
   justify-content: space-between;
+
+  ${mobile({ flexDirection: "column" })};
 `;
 
 const Info = styled.div`
@@ -53,6 +59,8 @@ const Info = styled.div`
 const Product = styled.div`
   display: flex;
   justify-content: space-between;
+
+  ${mobile({ flexDirection: "column" })};
 `;
 const ProductDetail = styled.div`
   flex: 2;
@@ -95,10 +103,13 @@ const ProductAmountContainer = styled.div`
 const ProductAmount = styled.div`
   font-size: 24px;
   margin: 5px;
+
+  ${mobile({ margin: "5px 15px" })};
 `;
 const ProductPrice = styled.div`
   font-size: 30px;
   font-weight: 200;
+  ${mobile({ marginBottom: "15px" })};
 `;
 
 const Hr = styled.hr`
