0b543dab374b51449355c35031604d0e6ec212a3
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-01 19:07:24 +0530

    Add Routing using react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,34 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import ProductList from "./pages/ProductList";
 import Register from "./pages/Register";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  redirect,
+  Route,
+  Routes,
+} from "react-router-dom";
 
 const App = () => {
-  return <Cart />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to="/" /> : <Register />}
+        />
+        <Route path="/products?/:category" element={<ProductList />} />
+        <Route path="/product?/:productId" element={<Product />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
