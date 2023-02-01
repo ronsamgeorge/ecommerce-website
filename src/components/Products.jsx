589231c1441f7ebe5769d0b67bdd4b15da3fcb66
Ronald George <ronaldsamgeorge@gmail.com>
2023-02-02 00:59:55 +0530

    Add filter and sort for products

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,6 +1,9 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import styled from "styled-components";
 import { popularProducts } from "../data";
 import Product from "./Product";
+import axios from "axios";
 
 const Container = styled.div`
   padding: 20px;
@@ -8,10 +11,57 @@ const Container = styled.div`
   flex-wrap: wrap;
   justify-content: space-around;
 `;
-const Products = () => {
+const Products = ({ category, filters, sort }) => {
+  const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    const getProducts = async () => {
+      try {
+        const res = await axios.get(
+          category
+            ? `http://localhost:8080/api/products?category=${category}`
+            : `http://localhost:8080/api/products`
+        );
+        setProducts(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getProducts();
+  }, [category]);
+
+  // Filter Products on the basis of filters object selected
+  useEffect(() => {
+    category &&
+      setFilteredProducts(
+        products.filter((item) =>
+          Object.entries(filters).every(([key, value]) =>
+            item[key].includes(value)
+          )
+        )
+      );
+  }, [products, category, filters]);
+
+  useEffect(() => {
+    if (sort === "newest") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.createdAt - b.createdAt)
+      );
+    } else if (sort === "asc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => a.price - b.price)
+      );
+    } else if (sort === "dsc") {
+      setFilteredProducts((prev) =>
+        [...prev].sort((a, b) => b.price - a.price)
+      );
+    }
+  }, [sort]);
+
   return (
     <Container>
-      {popularProducts.map((item) => (
+      {filteredProducts.map((item) => (
         <Product item={item} key={item.id} />
       ))}
     </Container>
--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -5,6 +5,9 @@ import Products from "../components/Products";
 import Newsletter from "../components/Newsletter";
 import Footer from "../components/Footer";
 import { mobile } from "../responsive";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { FitnessCenter } from "@material-ui/icons";
 
 const Container = styled.div``;
 
@@ -38,6 +41,21 @@ const Select = styled.select`
 const Option = styled.option``;
 
 const ProductList = () => {
+  const location = useLocation();
+  const category = location.pathname.split("/")[2]; // get the category from the url pathname
+  const [filters, setFilter] = useState({}); // filter for modifying the search result, Empty at the beginning as no Filter is applied
+  const [sort, setSort] = useState("newest"); // sort the result items, by default newest first
+
+  // Updates the filter state when one is selected
+  const handleFilters = (event) => {
+    const value = event.target.value;
+    setFilter({
+      ...filters,
+      [event.target.name]: value,
+    });
+  };
+
+  console.log(filters);
   return (
     <Container>
       <Navbar />
@@ -46,19 +64,19 @@ const ProductList = () => {
       <FilterContainer>
         <Filter>
           <FilterText>Filter Products</FilterText>
-          <Select>
+          <Select name="color" onChange={handleFilters}>
             <Option disabled selected>
               Color
             </Option>
-            <Option>White</Option>
-            <Option>Black</Option>
-            <Option>Red</Option>
-            <Option>Blue</Option>
-            <Option>Yellow</Option>
-            <Option>Green</Option>
+            <Option>white</Option>
+            <Option>black</Option>
+            <Option>red</Option>
+            <Option>blue</Option>
+            <Option>yellow</Option>
+            <Option>green</Option>
           </Select>
 
-          <Select>
+          <Select name="size" onChange={handleFilters}>
             <Option disabled selected>
               Size
             </Option>
@@ -71,15 +89,17 @@ const ProductList = () => {
         </Filter>
         <Filter>
           <FilterText>Sort Products</FilterText>
-          <Select>
-            <Option selected>Newest</Option>
-            <Option>Price (asc)</Option>
-            <Option>Price (desc)</Option>
+          <Select onChange={(e) => setSort(e.target.value)}>
+            <Option value="newest" selected>
+              Newest
+            </Option>
+            <Option value="asc">Price (asc)</Option>
+            <Option value="dsc">Price (desc)</Option>
           </Select>
         </Filter>
       </FilterContainer>
 
-      <Products />
+      <Products category={category} filters={filters} sort={sort} />
       <Newsletter />
       <Footer />
     </Container>
