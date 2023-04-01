90477ab1478725c72b89acf78a4b2ddd913b92fa
Ronald George <ronaldsamgeorge@gmail.com>
2023-04-01 16:07:05 +0530

    Base Url Modify

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = "/api";
 const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
   .currentUser.accessToken;
 
