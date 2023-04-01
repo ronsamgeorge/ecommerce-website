7f6273ce18d462700f2af1bf333dc383a773a9b5
Ronald George <ronaldsamgeorge@gmail.com>
2023-04-01 16:16:46 +0530

    User empty check

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "/api";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root")).user)
+const BASE_URL = "/api/";
+const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
   .currentUser.accessToken;
 
 export const publicRequest = axios.create({
