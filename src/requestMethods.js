9011ffb43689ebf5921d812b8bf8d7c6a1202b2d
Ronald George <ronaldsamgeorge@gmail.com>
2023-04-01 19:50:19 +0530

    Check user present before assigning

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 
 const BASE_URL = "/api/";
-const TOKEN = JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
-  .currentUser.accessToken;
+const TOKEN = localStorage.getItem("persist:root")?.user
+  ? JSON.parse(JSON.parse(localStorage.getItem("persist:root"))?.user)
+      .currentUser.accessToken
+  : null;
 
 export const publicRequest = axios.create({
   baseURL: BASE_URL,
